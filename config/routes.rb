4e8a60aedd722611e5006b13cd7cf46497be81c1
miyagawao <ec2-user@ip-172-31-31-224.ap-northeast-1.compute.internal>
2023-02-15 11:40:22 +0000

    課題3

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :books, only: [:new, :create, :index, :show]
+  root to: 'homes#top'
+  get 'homes/about' =>'homes#about', as: 'about'
+  devise_for :users
+
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
