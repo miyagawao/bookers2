693d71f1b8b5d13ccac9168f183b55bfd4ad13bf
miyagawao <ec2-user@ip-172-31-31-224.ap-northeast-1.compute.internal>
2023-02-15 15:43:43 +0000

    [Add]ユーザー詳細ページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
@@ -11,13 +11,39 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
   
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :edit
+    end
+  end
+  
+  
+  private
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :books, only: [:new, :create, :index, :show]
+  resources :users, only: [:show, :edit]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   root to: 'homes#top'
   get 'homes/about' =>'homes#about', as: 'about'
   devise_for :users
