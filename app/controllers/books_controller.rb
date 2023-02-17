8a569a8bd6cc14a2ca1f2a24fe0f0d908b7d4a52
miyagawao <ec2-user@ip-172-31-31-224.ap-northeast-1.compute.internal>
2023-02-17 08:21:18 +0000

    [Add]userページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,15 +7,18 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to book_path(@book.id)
   end
 
   def index
     @books = Book.all
+
   end
 
   def show
     @book = Book.find(params[:id])
+    @user = current_user
+    @books = Book.all
   end
 
   def destroy
