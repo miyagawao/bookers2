ef962654de5433c446e8655d1248ded1fa1a09d7
miyagawao <ec2-user@ip-172-31-31-224.ap-northeast-1.compute.internal>
2023-02-25 05:07:51 +0000

    [Add]検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' =>'homes#about', as: 'about'
+  get 'search' => 'searches#search'
  
 
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
